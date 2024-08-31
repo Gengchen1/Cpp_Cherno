cc6b18ac373c47a5df03315fc169cbbbafede61a
Zhaogengchen <zhaogengchen5@gmail.com>
2024-08-31 12:41:27 +0300

    mutable关键字的两个用法

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <string>
+
+class Entity
+{
+private:
+    std::string m_Name;
+    mutable int m_DebugCount = 0; // 所以把类成员变量标记为mutable,意味着类中的const方法可以修改这个成员。
+public:
+    const std::string& GetName() const
+    {
+        m_DebugCount++;
+        return m_Name;
+    }
+};
 
 int main()
 {
+    const Entity e;
+    e.GetName();
+
+    int x = 0;
+    auto f = [=]() mutable
+    {
+        x++;
+        std::cout << "Hello" << std::endl;
+    };
+
+    f();
 
     std::cin.get();
 };
