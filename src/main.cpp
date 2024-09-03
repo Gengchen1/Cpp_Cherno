e54dba90a7a0f4d041b16aba5a03dd3191d87bae
Zhaogengchen <zhaogengchen5@gmail.com>
2024-09-03 12:22:14 +0300

    添加了一个解释为什么要用初始化列表初始化成员变量的例子

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
 
+class Example
+{
+public:
+    Example()
+    {
+        std::cout << "Created Entity !"<< std::endl; 
+    }
+
+    Example(int x)
+    {
+        std::cout << "Created Entity with" << x << "!" << std::endl;
+    }
+};
+
 class Entity
 {
 private:
     std::string m_Name;
     int m_Score;
+    Example m_Example;
 public:
-    // Entity()
+    // Entity()// 创建两次Example对象
     // {
     //     m_Name = "Unknown";
+    //     m_Example = Example(8);
     // }
     // Entity(const std::string &name)
     // {
     //     m_Name = name;
     // }
     Entity()
-        : m_Name("Unknown"), m_Score(0)
+        : m_Name("Unknown"), m_Score(0), m_Example(Example(8))
     {
     }
     Entity(const std::string &name)
@@ -29,10 +45,10 @@ public:
 int main()
 {
     Entity e0;
-    std::cout << e0.GetName() << std::endl;
+    // std::cout << e0.GetName() << std::endl;
 
-    Entity e1("Cherno");
-    std::cout << e1.GetName() << std::endl;
+    // Entity e1("Cherno");
+    // std::cout << e1.GetName() << std::endl;
 
     std::cin.get();
 };
