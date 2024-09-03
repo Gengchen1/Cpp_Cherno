8cd1ef689fe29836fe3ff8b269fe484b61bd4dd6
Zhaogengchen <zhaogengchen5@gmail.com>
2024-09-03 16:38:24 +0300

    三云运算符的用法

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+static int s_Level = 8;
+static int s_Speed = 2;
+
+
 int main()
 {
+    // if (s_Level > 5)
+    //     s_Speed = 10;
+    // else
+    //     s_Speed = 5;
+    // s_Speed = s_Level > 5 ? 10 : 5; // 在根据一种特定条件给变量赋值时使用
+    s_Speed = s_Level > 5 && s_Level < 10 ? s_Level > 10 ? 15 : 10 : 5; // 三元算符的嵌套
+    // 尽量不要向上面这样做嵌套。
+    // std::string rank = s_Level > 10 ? "Master" : "Beginner";
+
+    std::cout << s_Speed << std::endl;
 
     std::cin.get();
 };
