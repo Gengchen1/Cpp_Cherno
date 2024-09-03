cd5a16088bb537a3bcdbe911ddac3f8b121290de
Zhaogengchen <zhaogengchen5@gmail.com>
2024-09-03 11:22:26 +0300

    初始化列表初始化成员变量

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
 
+class Entity
+{
+private:
+    std::string m_Name;
+    int m_Score;
+public:
+    // Entity()
+    // {
+    //     m_Name = "Unknown";
+    // }
+    // Entity(const std::string &name)
+    // {
+    //     m_Name = name;
+    // }
+    Entity()
+        : m_Name("Unknown"), m_Score(0)
+    {
+    }
+    Entity(const std::string &name)
+        : m_Name(name), m_Score(0)
+    {
+    }
+
+    const std::string &GetName() const { return m_Name; }
+};
+
 int main()
 {
+    Entity e0;
+    std::cout << e0.GetName() << std::endl;
+
+    Entity e1("Cherno");
+    std::cout << e1.GetName() << std::endl;
 
     std::cin.get();
 };
