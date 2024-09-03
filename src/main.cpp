2461b7973a55e79db47fc9f00d8edce785dbb92d
Zhaogengchen <zhaogengchen5@gmail.com>
2024-09-04 00:31:35 +0300

    new 关键字

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
+using String = std::string;
+
+class Entity
+{
+private:
+    String m_Name;
+public:
+    Entity() : m_Name("Unknown") {}
+    Entity(const String& name) : m_Name(name) {}
+
+    const String& GetName() const { return m_Name; }
+};
+
 int main()
 {
+    int a =2;
+    int* b = new int[50]; // 200 bytes
+
+    Entity* entity = new Entity();
+    Entity* e1 = new(b) Entity();
+    Entity* e = (Entity*)malloc(sizeof(Entity));
 
     std::cin.get();
+    delete entity; // 调用了C函数free；
 };
