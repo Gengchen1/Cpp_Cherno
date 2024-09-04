208a96ca9e407256daed5ddf86dc06b80af3c6af
Zhaogengchen <zhaogengchen5@gmail.com>
2024-09-04 21:34:25 +0300

    隐式转换和隐式构造函数的存在

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <string>
 
+class Entity
+{
+private:
+    std::string m_Name;
+    int m_Age;
+public:
+    Entity(const std::string& name)
+        : m_Name(name), m_Age(-1) {}
+    // Entity(int age)
+    //     : m_Name("Unknown"), m_Age(age) {}
+    explicit Entity (int age)
+        : m_Name("Unknown"), m_Age(age) {}
+
+};
 
 int main()
 {
-    
+    // Entity a = 22; // ok
+    Entity a = (Entity)22;
+    Entity b = std::string("Cherno"); // error 
+    Entity b = "Cherno"; // error "Cherno" 是一个const char*类型,不是string 
+    Entity a(22); 
+    Entity b("Cherno");
+
+
     std::cin.get();
 }
