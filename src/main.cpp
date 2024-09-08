0255d1e5cc1ce85106f19017d58e5c28acb5f04b
Zhaogengchen <zhaogengchen5@gmail.com>
2024-09-08 17:51:54 +0300

    标准库vector动态数组的初级使用

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
+struct Vertex
+{
+    float x, y, z;
+};
+
+std::ostream& operator<<(std::ostream& stream, const Vertex& vertex)
+{
+    stream << vertex.x << ", " << vertex.y << ", " << vertex.z;
+    return stream;
+}
 
 int main()
 {
-    
+    std::vector<Vertex> vertices;
+    vertices.push_back({1, 2, 3});
+    vertices.push_back({4, 6, 7});
+
+    // for (int i = 0; i < vertices.size(); i++)
+    //     std::cout << vertices[i] << std::endl;
+    vertices.erase(vertices.begin() + 1);
+    // vertices.clear();
+    for (Vertex& v : vertices) // 对于vertices中的每一个元素，将其值被Vertex类型的变量v所引用.
+        std::cout << v << std::endl;
+
     std::cin.get();
 }
