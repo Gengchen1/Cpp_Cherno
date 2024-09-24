2299e611c78983e8118872e8763924c47cec34df
Zhaogengchen <zhaogengchen5@gmail.com>
2024-09-24 12:02:32 +0300

    C++标准库静态数组

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <array>
+
+template<typename T, int size>
+void Printarray(const std::array<T, size>& data) {
+    for (int i = 0; i < size; i++) {
+        std::cout << data[i] << std::endl;
+    }
+}
 
 
 int main()
 {
-    
+    std::array<int, 5> Data = {1, 2, 3, 4, 5};
+    Printarray<int, 5>(Data); 
     std::cin.get();
 }
