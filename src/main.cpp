ce164324d1b35b789f1b206ee8f8c255fa0a2299
Zhaogengchen <zhaogengchen5@gmail.com>
2024-10-05 21:10:50 +0300

    C++内置排序函数Sort的使用

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
 
 
 int main()
 {
-    
+    std::vector<int> values = {1, 4, 2, 5, 3}; 
+    std::sort(values.begin(), values.end(),[](int a, int b) {
+        if (a == 1) 
+            return false;
+        if (b == 1)
+            return true;
+        return a < b; 
+    }); // 不给第三个参数，对于整数默认是升序
+
+
+    for (int value : values) 
+        std::cout << value << std::endl;
+
     std::cin.get();
 }
