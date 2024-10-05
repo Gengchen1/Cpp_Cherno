3f23a1fd791221ceb2b51ffe7a78e810512f5ec0
Zhaogengchen <zhaogengchen5@gmail.com>
2024-10-05 23:04:19 +0300

    类型双关,就是把已拥有的一段内存当作其他类型的内存来对待

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
 
+struct Entity{
+    int x, y;
+    int* GetPosition() {
+        return &x;
+    }
+};
+
 int main()
 {
+    Entity e = {5, 8};
+    // int *position = (int*)&e;
+    int *position = e.GetPosition();
+    std::cout << position[0] << "," << position[1] << std::endl;
+    int y = *(int*)((char*)&e + 4);
+    std::cout << y << std::endl;
+    int a = 50;
+    // double value = a; // 隐式转换
+    double value = *(double*)&a; 
+    std::cout << value << std::endl;
     
     std::cin.get();
 }
